8ddc6ea7705f785ab5dfb734c4c3de424151a38c
julien chevalier <chevalier.j@outlook.com>
2020-01-25 07:00:45 +0100

    add data

---
+++ data/shaders/compile.bat
@@ -0,0 +1,4 @@
+C:/VulkanSDK/1.1.130.0/Bin/glslc.exe shader.vert -o shader.vso
+C:/VulkanSDK/1.1.130.0/Bin/glslc.exe shader.frag -o shader.fso
+C:/VulkanSDK/1.1.130.0/Bin/glslc.exe tile.vert -o tile.vso
+C:/VulkanSDK/1.1.130.0/Bin/glslc.exe tile.frag -o tile.fso
