c332ae5643803a062d69aeda6f9342a19f025e86
julien chevalier <chevalier.j@outlook.com>
2020-02-10 06:49:56 +0100

    Update Tutorial_02.cpp

--- src/Tutorial_02/Tutorial_02.cpp
+++ src/Tutorial_02/Tutorial_02.cpp
@@ -5,19 +5,19 @@
    - les boucles
 */
 
-// Pour gérer l'aléatoire, j'ai besoin des fichiers "cstdlib" et "ctime" que je rajoute à l'aide de l'instruction "#include".
+// Pour gérer l'aléatoire, on a besoin des fichiers "cstdlib" et "ctime" qu'on rajoute à l'aide de l'instruction "#include".
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
 
 int main()
 {
-	// Je crée des variables pour contenir le score des deux opposants.
+	// On crée des variables pour contenir le score des deux opposants.
 	int score_joueur = 0;
 	int score_PC = 0;
 
-	// J'initialise le générateur de nombres aléatoires avec la fonction "srand".
-	// J'utilise "time" pour récupérer leur courante.
+	// On initialise le générateur de nombres aléatoires avec la fonction "std::srand".
+	// On utilise "std::time" pour récupérer leur courante.
 	// De cette manière, la séquence de nombres aléatoire sera différente à chaque lancement du jeu.
 	std::srand(std::time(nullptr));
 
@@ -28,15 +28,16 @@ int main()
 	std::cout << "1 => caillou" << std::endl;
 	std::cout << "2 => ciseaux" << std::endl;
 
-	// L'instruction "while" démare une boucle.
-	// Les instructions contenues dans le bloc de code suivant vont être répété "tant que" la condition entre les parenthèse est "vraie".
-	//
-	// while (<condition>)
-	// {
-	//    <code_répété>
-	// }
-	//
-	// Dans notre cas, le conditionest toujours vraie. C'est une boucle infinie.
+	/* L'instruction "while" démarre une boucle.
+	   Les instructions contenues dans le bloc de code suivant vont être répété "tant que" la condition entre les parenthèse est "vraie".
+	  
+	   while (<condition>)
+	   {
+	      <code_répété>
+	   }
+	  
+	   Dans notre cas, le condition est toujours vraie. C'est une boucle infinie.
+	*/
 	while (true)
 	{
 		std::cout << "Papier, Caillou, Ciseaux !" << std::endl;
@@ -48,6 +49,7 @@ int main()
 		std::cin >> choix_joueur;
 
 		// Si le choix du joueur est un nombre négatif, on appelle l'instruction "break" qui permet de sortir de la boucle.
+		// De cette manière le joueur décide quand quitter le jeu.
 		if (choix_joueur < 0)
 		{
 			break;
@@ -56,13 +58,14 @@ int main()
 		// Pour le choix du PC, on utilise "std::rand" qui renvoit un nombre aléatoire.
 		// Comme on veut un nombre entre 0 et 2, on utilise l'opérateur "%" ou "modulo".
 		// Il renvoit le reste de la division entière du nombre à gauche par le nombre à droite.
+		// Par exemple : 13%3 renvoit 1 . 13 = 4 x 3 + 1 . 1 est le reste de la division 13 par 3.
 		int choix_PC = std::rand()%3;
 
 		std::cout << "choix PC " << choix_PC << std::endl;
 
-		// Enfin on utilise les structures de contrôle pour mettre à jour le score.
+		// Enfin on utilise les structures de contrôle pour déterminer qui gagne/perd et mettre à jour le score.
 		// Ca fait beaucoup de "if" ^^.
-		// Il existe une solution plus simole avec moins de répétitions de code.
+		// Il existe une solution plus simple avec moins de répétitions de code.
 		if (choix_joueur == choix_PC)
 		{
 			std::cout << "Egalité !" << std::endl;
@@ -116,7 +119,6 @@ int main()
 		}
 
 		std::cout << "Score : Joueur " << score_joueur << " PC " << score_PC << std::endl;
-
 	}
 
 	std::cout << "Game over !" << std::endl;
